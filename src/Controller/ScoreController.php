baa747dfc8b4bb5afa27e43721cc2593e6ec9004
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2021-07-03 11:04:14 +0200

    Saving term to database

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\SearchPopularityScore;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,10 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class ScoreController extends AbstractController
 {
     private $searchPopularityScore;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
-    public function __construct(SearchPopularityScore $searchPopularityScore)
+    public function __construct(SearchPopularityScore $searchPopularityScore, EntityManagerInterface $entityManager)
     {
         $this->searchPopularityScore = $searchPopularityScore;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -23,6 +29,9 @@ class ScoreController extends AbstractController
     public function index(Request $request): Response
     {
         $score = $this->searchPopularityScore->search($request->query->get('term'));
+
+        $this->entityManager->persist($score);
+        $this->entityManager->flush();
 
         return $this->json([
             'term' => $score->getTerm(),
--- tests/Feature/GettingPopularityScoreTest.php
+++ tests/Feature/GettingPopularityScoreTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Feature;
 
 use App\Entity\SearchResult;
+use App\Entity\SearchTermPopularityScore;
 use App\Service\GithubProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -26,6 +27,8 @@ class GettingPopularityScoreTest extends WebTestCase
 
         self::getContainer()->set('App\Service\Provider', $provider);
 
+        $dm = $this->getEntityManagerAndPrepareDatabase();
+
         // Request a specific page
         $crawler = $client->request('GET', '/score?term='.$searchTerm);
 
@@ -36,5 +39,51 @@ class GettingPopularityScoreTest extends WebTestCase
 
         $this->assertEquals('php', $responseData['term']);
         $this->assertEquals(4, $responseData['score']);
+    }
+
+    /** @test */
+    public function it_saves_term_popularity_to_database()
+    {
+        $searchTerm = 'php';
+
+        $provider = $this->createMock(GithubProvider::class);
+        $provider->expects($this->any())
+            ->method('handle')
+            ->will(
+                $this->onConsecutiveCalls(
+                    new SearchResult($searchTerm, 20),
+                    new SearchResult($searchTerm, 30)
+                ));
+
+        $client = static::createClient();
+
+        self::getContainer()->set('App\Service\Provider', $provider);
+
+        $dm = $this->getEntityManagerAndPrepareDatabase();
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/score?term=' . $searchTerm);
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+
+        $searchTermPopularityScore = $dm->getRepository(SearchTermPopularityScore::class)
+            ->findOneBy([
+                'term' => $searchTerm,
+            ]);
+        $this->assertNotNull($searchTermPopularityScore, 'Search term popularity score is not saved.');
+    }
+
+    /**
+     * @return object|null
+     */
+    public function getEntityManagerAndPrepareDatabase(): ?object
+    {
+        $dm = self::getContainer()->get('doctrine.orm.default_entity_manager');
+
+        $connection = $dm->getConnection();
+        $platform = $connection->getDatabasePlatform();
+        $connection->executeUpdate($platform->getTruncateTableSQL('search_term_popularity_score', true /* whether to cascade */));
+        return $dm;
     }
 }
